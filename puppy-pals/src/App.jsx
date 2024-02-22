cad68d78042e9a4d4edad68f2ca7d73d2d369686
DevLinx76 <126533274+DevLinx76@users.noreply.github.com>
2024-02-22 14:36:45 -0500

    Updated src/App.jsx

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 1. Import the `useState` Hook
+import { useState } from 'react';
+
+// 2. Accessing the `puppyList` Array
+import { puppyList } from './data.js';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 4. Using the `useState` Hook with Default Value
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // 3. Logging `puppyList` to the Console
+  console.log(puppies);
+
+
+
+  // 5. Handle Click Events
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="App">
+      {/* 6. Mapping Over the `puppies` Array */}
+      {puppies.map(puppy => {
+        return (
+          // Using `key` prop for each puppy based on their unique id.
+          <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+          {puppy.name}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        );
+      })}
+      
+      {/* 7. Conditional Rendering Based on State */}
+      {featPupId && <p>Featured Puppy ID: {featPupId}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
